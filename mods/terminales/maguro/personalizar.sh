27ff66ef7f2ac5acf66db5ceb7c6b8cd37f955da
superatmel <superatmel@gmail.com>
2012-02-15 10:15:25 +0100

    maguro:Do not clean inputmethods
    Change-Id: I5a8b9d04e33ad4ef73c0173c4086372b8e05055a

--- mods/terminales/maguro/personalizar.sh
+++ mods/terminales/maguro/personalizar.sh
@@ -4,8 +4,8 @@ msgStatus "Personalizacion exclusiva"
 msgStatus " "
 rm -rf $OUT/repack.d/ota/system/app/BooksPhone.apk
 rm -rf $OUT/repack.d/ota/system/app/GlobalTime.apk
-rm -rf $OUT/repack.d/ota/system/app/OpenWnn.apk
-rm -rf $OUT/repack.d/ota/system/app/PinYinIME.apk
+#rm -rf $OUT/repack.d/ota/system/app/OpenWnn.apk
+#rm -rf $OUT/repack.d/ota/system/app/PinYinIME.apk
 rm -rf $OUT/repack.d/ota/system/app/SpareParts.apk
 rm -rf $OUT/repack.d/ota/system/app/SpareParts2.apk
 rm -rf $OUT/repack.d/ota/system/app/SpeechRecorder.apk
