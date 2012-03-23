e8c734c4550ffae7ad6cc45c9a07897d757c27fe
cmartinezlozano <carlosmartinezlozano@gmail.com>
2012-03-23 12:31:50 +0100

    Added all needed for Nvsbl sirius
    Change-Id: I0f57c800747cd31f221028954d46534e2b9093de

---
+++ mods/terminales/sirius/personalizar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+. mensajes.sh
+msgStatus "Personalizacion exclusiva"
+msgStatus " "
+rm -rf $OUT/repack.d/ota/system/app/BooksPhone.apk
+rm -rf $OUT/repack.d/ota/system/app/GlobalTime.apk
+rm -rf $OUT/repack.d/ota/system/app/SpareParts.apk
+rm -rf $OUT/repack.d/ota/system/app/SpareParts2.apk
+rm -rf $OUT/repack.d/ota/system/app/SpeechRecorder.apk
+rm -rf $OUT/repack.d/ota/system/etc/super/04superteam
+rm $OUT/repack.d/ota/boot.img
