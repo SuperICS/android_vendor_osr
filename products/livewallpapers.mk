e3f4fe32b61108c6cbf0c6e77aef384daa438940
podxboq <podxboq@gmail.com>
2012-03-29 15:56:48 +0200

    Add live wallpapers programs

--- products/livewallpapers.mk
+++ products/livewallpapers.mk
@@ -3,7 +3,12 @@ PRODUCT_PACKAGES += \
         LiveWallpapers \
         LiveWallpapersPicker \
         VisualizationWallpapers \
-        librs_jni
+        librs_jni \
+        Galaxy4 \
+        HoloSpiralWallpaper \
+        MagicSmokeWallpapers \
+        NoiseField \
+        PhaseBeam
 
 PRODUCT_COPY_FILES += \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
