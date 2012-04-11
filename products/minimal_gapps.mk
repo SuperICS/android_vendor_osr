d0f8d32cb8dd1ef5967452fafed293df06a958f4
podxboq <podxboq@gmail.com>
2012-04-11 13:57:50 +0200

    Add SetupWizard to minimal gapps

--- products/minimal_gapps.mk
+++ products/minimal_gapps.mk
@@ -9,4 +9,5 @@ vendor/osr/prebuilt/gapps/app/GoogleCalendarSyncAdapter.apk:system/app/GoogleCal
 vendor/osr/prebuilt/gapps/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
 vendor/osr/prebuilt/gapps/app/MarketUpdater.apk:system/app/MarketUpdater.apk \
 vendor/osr/prebuilt/gapps/app/OneTimeInitializer.apk:system/app/OneTimeInitializer.apk \
+vendor/osr/prebuilt/gapps/app/SetupWizard.apk:system/app/SetupWizard.apk \
 vendor/osr/prebuilt/gapps/app/Talk.apk:system/app/Talk.apk 
