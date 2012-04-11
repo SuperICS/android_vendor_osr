80b9e74be5246a300e831609db4dca04aabd4c05
cmartinezlozano <carlosmartinezlozano@gmail.com>
2012-04-11 23:24:46 +0200

    SuperOSR 1.4.0 para Sirius y gapps
    Change-Id: Idbd8bfb69e442088d58c504e4928a593caaba726

--- products/minimal_gapps.mk
+++ products/minimal_gapps.mk
@@ -1,12 +1,29 @@
 #apps
 PRODUCT_COPY_FILES += \
-vendor/osr/prebuilt/gapps/app/Vending.apk:system/app/Vending.apk \
-vendor/osr/prebuilt/gapps/app/GoogleLoginService.apk:system/app/GoogleLoginService.apk \
-vendor/osr/prebuilt/gapps/app/GooglePackageVerifier.apk:system/app/GooglePackageVerifier.apk \
-vendor/osr/prebuilt/gapps/app/GooglePackageVerifierUpdater.apk:system/app/GooglePackageVerifierUpdater.apk \
-vendor/osr/prebuilt/gapps/app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
-vendor/osr/prebuilt/gapps/app/GoogleCalendarSyncAdapter.apk:system/app/GoogleCalendarSyncAdapter.apk \
-vendor/osr/prebuilt/gapps/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
-vendor/osr/prebuilt/gapps/app/MarketUpdater.apk:system/app/MarketUpdater.apk \
-vendor/osr/prebuilt/gapps/app/OneTimeInitializer.apk:system/app/OneTimeInitializer.apk \
-vendor/osr/prebuilt/gapps/app/Talk.apk:system/app/Talk.apk 
+    vendor/osr/prebuilt/gapps/app/Vending.apk:system/app/Vending.apk \
+    vendor/osr/prebuilt/gapps/app/GoogleLoginService.apk:system/app/GoogleLoginService.apk \
+    vendor/osr/prebuilt/gapps/app/GooglePackageVerifier.apk:system/app/GooglePackageVerifier.apk \
+    vendor/osr/prebuilt/gapps/app/GooglePackageVerifierUpdater.apk:system/app/GooglePackageVerifierUpdater.apk \
+    vendor/osr/prebuilt/gapps/app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
+    vendor/osr/prebuilt/gapps/app/GoogleCalendarSyncAdapter.apk:system/app/GoogleCalendarSyncAdapter.apk \
+    vendor/osr/prebuilt/gapps/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
+    vendor/osr/prebuilt/gapps/app/MarketUpdater.apk:system/app/MarketUpdater.apk \
+    vendor/osr/prebuilt/gapps/app/OneTimeInitializer.apk:system/app/OneTimeInitializer.apk \
+    vendor/osr/prebuilt/gapps/app/Talk.apk:system/app/Talk.apk \
+    vendor/osr/prebuilt/gapps/app/Gmail.apk:system/app/Gmail.apk \
+    vendor/osr/prebuilt/gapps/app/VoiceSearch.apk:system/app/VoiceSearch.apk \
+    vendor/osr/prebuilt/gapps/app/YouTube.apk:system/app/YouTube.apk \
+    vendor/osr/prebuilt/gapps/app/GenieWidget.apk:system/app/GenieWidget.apk \
+    vendor/osr/prebuilt/gapps/app/SetupWizard.apk:system/app/SetupWizard.apk \
+    vendor/osr/prebuilt/gapps/etc/contributors.css:system/etc/contributors.css \
+    vendor/osr/prebuilt/gapps/etc/resolv.conf:system/etc/resolv.conf \
+    vendor/osr/prebuilt/gapps/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
+    vendor/osr/prebuilt/gapps/etc/permissions/features.xml:system/etc/permissions/features.xml \
+    vendor/osr/prebuilt/gapps/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
+    vendor/osr/prebuilt/gapps/framework/com.google.android.maps.jar:system/framework/com.google.android.maps.jar \
+    vendor/osr/prebuilt/gapps/framework/com.google.android.media.effects.jar:system/framework/com.google.android.media.effects.jar \
+    vendor/osr/prebuilt/gapps/lib/libpicowrapper.so:system/lib/libpicowrapper.so \
+    vendor/osr/prebuilt/gapps/lib/libspeexwrapper.so:system/lib/libspeexwrapper.so \
+    vendor/osr/prebuilt/gapps/lib/libvideochat_jni.so:system/lib/libvideochat_jni.so \
+    vendor/osr/prebuilt/gapps/lib/libvideochat_stabilize.so:system/lib/libvideochat_stabilize.so \
+    vendor/osr/prebuilt/gapps/lib/libvoicesearch.so:system/lib/libvoicesearch.so
