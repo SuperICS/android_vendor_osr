2052c5aa1ef9d366e310a8d5a95322e9a2226fdd
nelo360 <nelo360@gmail.com>
2012-04-17 01:51:02 +0200

    galaxys2: Do not delte soundrecorder, adder fm as system app
    Change-Id: Iaee4e1f7bbb14544f23a3a7c32d204c2b8e4b11b

--- mods/terminales/galaxys2/personalizar.sh
+++ mods/terminales/galaxys2/personalizar.sh
@@ -7,6 +7,5 @@ rm -rf $OUT/repack.d/ota/system/app/GlobalTime.apk
 rm -rf $OUT/repack.d/ota/system/app/SpareParts.apk
 rm -rf $OUT/repack.d/ota/system/app/SpareParts2.apk
 rm -rf $OUT/repack.d/ota/system/app/SpeechRecorder.apk
-rm -rf $OUT/repack.d/ota/system/app/SoundRecorder2.apk
 rm -rf $OUT/repack.d/ota/system/app/VoiceDialer.apk
 rm -rf $OUT/repack.d/ota/system/app/PlusOne.apk
