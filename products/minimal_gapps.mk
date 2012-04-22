8fa8500717d4ca3591d6e2750c1b86b10b4246ca
superatmel <superatmel@gmail.com>
2012-04-23 01:37:27 +0200

    Reorder Gapps, Facelook, drm call, update gapps
    Change-Id: I4239ac4edb4fe2dd02dc944bafb4e76160e5d996

--- products/minimal_gapps.mk
+++ products/minimal_gapps.mk
@@ -1,26 +1,31 @@
 #apps
 PRODUCT_COPY_FILES += \
-    vendor/osr/prebuilt/gapps/app/Vending.apk:system/app/Vending.apk \
-    vendor/osr/prebuilt/gapps/app/GoogleLoginService.apk:system/app/GoogleLoginService.apk \
+    vendor/osr/prebuilt/gapps/app/CarHome.apk:system/app/CarHome.apk \
+    vendor/osr/prebuilt/gapps/app/ChromeBookmarksSyncAdapter.apk:system/app/ChromeBookmarksSyncAdapter.apk \
+    vendor/osr/prebuilt/gapps/app/Gallery2.apk:system/app/Gallery2.apk \
+    vendor/osr/prebuilt/gapps/app/GenieWidget.apk:system/app/GenieWidget.apk \
+    vendor/osr/prebuilt/gapps/app/Gmail.apk:system/app/Gmail.apk \
     vendor/osr/prebuilt/gapps/app/GoogleBackupTransport.apk:system/app/GoogleBackupTransport.apk \
-    vendor/osr/prebuilt/gapps/app/GooglePackageVerifier.apk:system/app/GooglePackageVerifier.apk \
-    vendor/osr/prebuilt/gapps/app/GooglePackageVerifierUpdater.apk:system/app/GooglePackageVerifierUpdater.apk \
-    vendor/osr/prebuilt/gapps/app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
     vendor/osr/prebuilt/gapps/app/GoogleCalendarSyncAdapter.apk:system/app/GoogleCalendarSyncAdapter.apk \
     vendor/osr/prebuilt/gapps/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
-    vendor/osr/prebuilt/gapps/app/GooglePartnerSetup.apk:system/app/GooglePartnerSetup.apk \
     vendor/osr/prebuilt/gapps/app/GoogleFeedback.apk:system/app/GoogleFeedback.apk \
+    vendor/osr/prebuilt/gapps/app/GoogleLoginService.apk:system/app/GoogleLoginService.apk \
+    vendor/osr/prebuilt/gapps/app/GooglePartnerSetup.apk:system/app/GooglePartnerSetup.apk \
+    vendor/osr/prebuilt/gapps/app/GoogleQuickSearchBox.apk:system/app/GoogleQuickSearchBox.apk \
+    vendor/osr/prebuilt/gapps/app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
+    vendor/osr/prebuilt/gapps/app/GoogleTTS.apk:system/app/GoogleTTS.apk \
+    vendor/osr/prebuilt/gapps/app/Maps.apk:system/app/Maps.apk \
     vendor/osr/prebuilt/gapps/app/MarketUpdater.apk:system/app/MarketUpdater.apk \
+    vendor/osr/prebuilt/gapps/app/MediaUploader.apk:system/app/MediaUploader.apk \
+    vendor/osr/prebuilt/gapps/app/Music.apk:system/app/Music.apk \
     vendor/osr/prebuilt/gapps/app/NetworkLocation.apk:system/app/NetworkLocation.apk \
     vendor/osr/prebuilt/gapps/app/OneTimeInitializer.apk:system/app/OneTimeInitializer.apk \
-    vendor/osr/prebuilt/gapps/app/Talk.apk:system/app/Talk.apk \
-    vendor/osr/prebuilt/gapps/app/Gmail.apk:system/app/Gmail.apk \
-    vendor/osr/prebuilt/gapps/app/VoiceSearch.apk:system/app/VoiceSearch.apk \
-    vendor/osr/prebuilt/gapps/app/YouTube.apk:system/app/YouTube.apk \
-    vendor/osr/prebuilt/gapps/app/GenieWidget.apk:system/app/GenieWidget.apk \
     vendor/osr/prebuilt/gapps/app/SetupWizard.apk:system/app/SetupWizard.apk \
-    vendor/osr/prebuilt/gapps/etc/contributors.css:system/etc/contributors.css \
-    vendor/osr/prebuilt/gapps/etc/resolv.conf:system/etc/resolv.conf \
+    vendor/osr/prebuilt/gapps/app/Street.apk:system/app/Street.apk \
+    vendor/osr/prebuilt/gapps/app/Talk.apk:system/app/Talk.apk \
+    vendor/osr/prebuilt/gapps/app/YouTube.apk:system/app/YouTube.apk \
+    vendor/osr/prebuilt/gapps/app/Vending.apk:system/app/Vending.apk \
+    vendor/osr/prebuilt/gapps/app/VoiceSearch.apk:system/app/VoiceSearch.apk \
     vendor/osr/prebuilt/gapps/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
     vendor/osr/prebuilt/gapps/etc/permissions/features.xml:system/etc/permissions/features.xml \
     vendor/osr/prebuilt/gapps/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
