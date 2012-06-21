72adb7934176214b19eff6d723d068dcebb90cd2
podxboq <podxboq@gmail.com>
2012-06-21 22:02:01 +0200

    Maps library is mandatory

--- products/tiny_gapps.mk
+++ products/tiny_gapps.mk
@@ -4,6 +4,8 @@ vendor/osr/prebuilt/gapps/app/GoogleLoginService.apk:system/app/GoogleLoginServi
 vendor/osr/prebuilt/gapps/app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
 vendor/osr/prebuilt/gapps/app/GoogleCalendarSyncAdapter.apk:system/app/GoogleCalendarSyncAdapter.apk \
 vendor/osr/prebuilt/gapps/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
+vendor/osr/prebuilt/gapps/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
+vendor/osr/prebuilt/gapps/framework/com.google.android.maps.jar:system/framework/com.google.android.maps.jar \
 vendor/osr/prebuilt/gapps/app/MarketUpdater.apk:system/app/MarketUpdater.apk \
 vendor/osr/prebuilt/gapps/app/OneTimeInitializer.apk:system/app/OneTimeInitializer.apk \
 vendor/osr/prebuilt/gapps/app/Phonesky.apk:system/app/Phonesky.apk \
